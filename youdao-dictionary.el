0c2d549e2d32061255856c987cd533a7f932b0b6
Chunyang Xu <xuchunyang56@gmail.com>
2015-01-20 19:42:28 +0800

    Update documentation

--- youdao-dictionary.el
+++ youdao-dictionary.el
@@ -23,6 +23,19 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
+;;
+;; A simple Youdao Dictionary interface for Emacs
+;;
+;; Below are commands you can use:
+;; `youdao-dictionary-search-at-point'
+;; Search word at point and display result with buffer
+;; `youdao-dictionary-search-at-point+'
+;; Search word at point and display result with popup-tip
+;; `youdao-dictionary-search-from-input'
+;; Search word from input and display result with buffer
+;; `youdao-dictionary-search-and-replace'
+;; Search word at point and display result with popup-menu, replace word with
+;; selected translation.
 
 ;;; Code:
 (require 'json)
