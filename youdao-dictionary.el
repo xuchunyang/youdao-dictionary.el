a745e3197cf3498244265efab8e1cc1f4fedcddc
Xu Chunyang <xuchunyang56@gmail.com>
2019-12-27 10:42:13 +0800

    Silence byte compiler warnings
    Notes on using names: When using the quote form of a variable, the name still
    has to be written in full form, in other words, the package prefix can't
    omitted.

--- youdao-dictionary.el
+++ youdao-dictionary.el
@@ -53,6 +53,8 @@
 (eval-when-compile (require 'names))
 
 (declare-function pdf-view-active-region-text "pdf-view" ())
+(declare-function pdf-view-active-region-p "pdf-view" ())
+(declare-function posframe-delete "posframe")
 
 (defgroup youdao-dictionary nil
   "Youdao dictionary interface for Emacs."
@@ -262,7 +264,7 @@ i.e. `[语][计] dictionary' => 'dictionary'."
               (mode)
               (insert (-format-result word))
               (goto-char (point-min))
-              (set (make-local-variable 'current-buffer-word) word)))
+              (set (make-local-variable 'youdao-dictionary-current-buffer-word) word)))
           (posframe-show buffer-name
                          :left-fringe 8
                          :right-fringe 8
@@ -276,7 +278,7 @@ i.e. `[语][计] dictionary' => 'dictionary'."
 (defun play-voice-of-current-word ()
   "Play voice of current word shown in *Youdao Dictionary*."
   (interactive)
-  (if (local-variable-if-set-p 'current-buffer-word)
+  (if (local-variable-if-set-p 'youdao-dictionary-current-buffer-word)
       (-play-voice current-buffer-word)))
 
 (define-derived-mode mode org-mode "Youdao-dictionary"
@@ -284,8 +286,8 @@ i.e. `[语][计] dictionary' => 'dictionary'."
 \\{youdao-dictionary-mode-map}"
   (read-only-mode 1)
   (define-key mode-map "q" 'quit-window)
-  (define-key mode-map "p" 'play-voice-of-current-word)
-  (define-key mode-map "y" 'play-voice-at-point))
+  (define-key mode-map "p" 'youdao-dictionary-play-voice-of-current-word)
+  (define-key mode-map "y" 'youdao-dictionary-play-voice-at-point))
 
 (defun -search-and-show-in-buffer (word)
   "Search WORD and show result in `youdao-dictionary-buffer-name' buffer."
@@ -296,7 +298,7 @@ i.e. `[语][计] dictionary' => 'dictionary'."
           (mode)
           (insert (-format-result word))
           (goto-char (point-min))
-          (set (make-local-variable 'current-buffer-word) word))
+          (set (make-local-variable 'youdao-dictionary-current-buffer-word) word))
         (unless (get-buffer-window (current-buffer))
           (switch-to-buffer-other-window buffer-name)))
     (message "Nothing to look up")))
